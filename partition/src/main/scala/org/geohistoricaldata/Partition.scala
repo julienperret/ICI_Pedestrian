49310cc0f400d664ab8b26dcf4f36968608b0915
Julien Perret <julien.perret@gmail.com>
2021-02-03 09:10:46 +0100

    Fixing the sidewalk bug and adding an objectid attribute

--- partition/src/main/scala/org/geohistoricaldata/Partition.scala
+++ partition/src/main/scala/org/geohistoricaldata/Partition.scala
@@ -35,28 +35,29 @@ object Partition extends App {
   def getWithAttributes(path: Path, attributes: Array[String]) = Utils.getShapefile(path).flatMap(f=>getFeaturePolygons(f).map(p=>(p, attributes.map(a=>f.getAttribute(a))))).filter(_._1.intersects(arrondissementGeom))
 
   //  val chaussee = Utils.getShapefile(new JFile("/home/julien/devel/ICI_Pedestrian/input_l93/plan-de-voirie-chaussees.shp").toPath).flatMap(getFeaturePolygons).filter(_.intersects(arrondissementGeom))
-  val chaussee = get(path("plan-de-voirie-chaussees"))
-  val chausseeGeom = reduce(arrondissementGeom.getFactory.createGeometryCollection(chaussee.toArray).union())
+  val chaussee = getWithAttributes(path("plan-de-voirie-chaussees"), Array("objectid"))
+  val chausseeGeom = reduce(arrondissementGeom.getFactory.createGeometryCollection(chaussee.map(_._1).toArray).union())
 
   // check that a geometry does not intersect the street (remove the 'quais de seine')
-  def notOnStreet(g: Geometry) = !g.intersects(chausseeGeom) || g.intersection(chausseeGeom).getArea < 0.1
+  def notOnStreet(g: Geometry) = !g.intersects(chausseeGeom) || g.intersection(chausseeGeom).getArea < 1.0
 
-  val trottoirs = get(path("plan-de-voirie-trottoirs-emprises")).filter(notOnStreet)
-  val passages = get(path("plan-de-voirie-passages-pietons"))
-  val mixte = get(path("plan-de-voirie-aires-mixtes-vehicules-et-pietons"))
-  val ilots = get(path("plan-de-voirie-ilots-directionnels"))
-  val terre_pleins = get(path("plan-de-voirie-terre-pleins"))
-  val escaliers = get(path("plan-de-voirie-voies-en-escalier"))
+  val trottoirs = getWithAttributes(path("plan-de-voirie-trottoirs-emprises"), Array("objectid")).filter(x=>notOnStreet(x._1))
+  val passages = getWithAttributes(path("plan-de-voirie-passages-pietons"), Array("objectid"))
+  val mixte = getWithAttributes(path("plan-de-voirie-aires-mixtes-vehicules-et-pietons"), Array("objectid"))
+  val ilots = getWithAttributes(path("plan-de-voirie-ilots-directionnels"), Array("objectid"))
+  val terre_pleins = getWithAttributes(path("plan-de-voirie-terre-pleins"), Array("objectid"))
+  val escaliers = getWithAttributes(path("plan-de-voirie-voies-en-escalier"), Array("objectid"))
 //  val batiments = getWithAttributes(path("volumesbatis"),Array("h_et_max"))
-  val espacesVerts = get(path("plan-de-voirie-emprises-espaces-verts"))
-  val batimentsIGN = getWithAttributes(path("BATIMENT_XY"), Array("HAUTEUR", "NB_ETAGES", "NB_LOGTS"))
-  val batimentsIGN_seq = batimentsIGN.map(_._1)
-  val features = FeaturePolygonizer.getPolygons((chaussee ++ trottoirs ++ passages ++ mixte ++ ilots ++ terre_pleins ++ escaliers ++ batimentsIGN_seq).toList).map {
+  val espacesVerts = getWithAttributes(path("plan-de-voirie-emprises-espaces-verts"), Array("objectid"))
+  val batimentsIGN = getWithAttributes(path("BATIMENT_XY"), Array("HAUTEUR", "NB_ETAGES", "NB_LOGTS", "ID"))
+  val inputFeatures = (chaussee ++ trottoirs ++ passages ++ mixte ++ ilots ++ terre_pleins ++ escaliers ++ batimentsIGN).toList.map(_._1)
+//  val batimentsIGN_seq = batimentsIGN.map(_._1)
+  val features = FeaturePolygonizer.getPolygons(inputFeatures).map {
     p =>
       val point = p.getInteriorPoint
-      case class PartitionType(name: String, seq: Seq[Polygon])
+      case class PartitionType(name: String, seq: Seq[(Polygon, Array[AnyRef])])
       val typeCollection = Array(
-        PartitionType("batiment", batimentsIGN_seq),
+        PartitionType("batiment", batimentsIGN),
         PartitionType("trottoir", trottoirs),
         PartitionType("passage piéton", passages),
         PartitionType("aire mixte véhicules et piétons", mixte),
@@ -67,7 +68,7 @@ object Partition extends App {
         PartitionType("espace vert", espacesVerts)
       )
       // for each polygon, get the first partition type that matches
-      typeCollection.find(_.seq.exists(_.contains(point))) match {
+      typeCollection.find(_.seq.exists(_._1.contains(point))) match {
         case Some(pt) => pt.name match {
           case n if n.equals("batiment") =>
             // if it is a building, add the height, etc.
@@ -77,18 +78,26 @@ object Partition extends App {
               case Some(x) => x.toString.toInt
               case None => 0
             }
-
-            (p, n, attributs.head.asInstanceOf[Double], getAsInt(attributs(1)), getAsInt(attributs(2)))
-          case n if n.equals("trottoir") || n.equals("ilot directionnel") || n.equals("terre-plein") => (p, n, 0.1, 0, 0) // arbitrary sidewalk height
-          case n => (p, n, 0.0, 0, 0)
+            (p, n, attributs.head.asInstanceOf[Double], getAsInt(attributs(1)), getAsInt(attributs(2)), attributs(3).toString)
+          case n if n.equals("trottoir") || n.equals("ilot directionnel") || n.equals("terre-plein") =>
+            val attributs = pt.seq.find(_._1.contains(point)).get._2
+            (p, n, 0.1, 0, 0, attributs.head.toString) // arbitrary sidewalk height
+          case n =>
+            val attributs = pt.seq.find(_._1.contains(point)).get._2
+            (p, n, 0.0, 0, 0, attributs.head.toString)
         }
-        case None => (p, "autre", 0.0, 0, 0)
+        case None => (p, "autre", 0.0, 0, 0, "NONE")
       }
   }
 
   val outputDir = File("../output")
   Utils.createShapefile(
     new JFile(outputDir + "/partition.shp"),
-    "geom:Polygon:srid=2154,label:String,HAUTEUR:Double,NB_ETAGES:Integer,NB_LOGTS:Integer",
-    features.filter(_._1.intersects(arrondissementGeom)).map(g => Array[AnyRef](g._1, g._2, java.lang.Double.valueOf(g._3), java.lang.Integer.valueOf(g._4), java.lang.Integer.valueOf(g._5))))
+    "geom:Polygon:srid=2154,label:String,HAUTEUR:Double,NB_ETAGES:Integer,NB_LOGTS:Integer,objectid:String",
+    features.filter(_._1.intersects(arrondissementGeom)).map(g => Array[AnyRef](g._1, g._2, java.lang.Double.valueOf(g._3), java.lang.Integer.valueOf(g._4), java.lang.Integer.valueOf(g._5), g._6)))
+  Utils.createShapefile(
+    new JFile(outputDir + "/partition_points.shp"),
+    "geom:Point:srid=2154,label:String,HAUTEUR:Double,NB_ETAGES:Integer,NB_LOGTS:Integer,objectid:String",
+    features.filter(_._1.intersects(arrondissementGeom)).map(g => Array[AnyRef](g._1.getInteriorPoint, g._2, java.lang.Double.valueOf(g._3), java.lang.Integer.valueOf(g._4), java.lang.Integer.valueOf(g._5), g._6)))
+
 }
